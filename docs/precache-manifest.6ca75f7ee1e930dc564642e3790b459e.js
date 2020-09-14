53d6098886e67b1cceac739405acddcf788ee32b
david7890 <davidco7890@gmail.com>
2020-09-13 19:47:27 -0500

    Produccion

---
+++ docs/precache-manifest.6ca75f7ee1e930dc564642e3790b459e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d0ba3f3959ad728b7e64afe496eb273",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9692c279c59bbf5cf06b",
+    "url": "/static/js/2.e9a086d8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e9a086d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c59e37f3a9afadc88fed",
+    "url": "/static/js/main.ed45da73.chunk.js"
+  },
+  {
+    "revision": "94d7ca25d4df19621824",
+    "url": "/static/js/runtime-main.b7f813af.js"
+  }
+]);
